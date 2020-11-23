599cd8c9bc44d6d154a26271c540a148260b1da3
Kechis1 <danielbill95@gmail.com>
2020-11-23 19:01:30 +0100

    Print cost of operator and starting cost of query plan for EXPLAIN statement

--- tests/myTest.c
+++ tests/myTest.c
@@ -11,7 +11,7 @@
 int main() {
     sqlite3 *db;
     int rc;
-    char *zDbName = "/Users/danielbill/sqlite/databases/test.db";
+    char *zDbName = "C:/Users/bil0104/Desktop/test.db";
     printf("open connection\n");
     rc = sqlite3_open_v2(zDbName, &db, SQLITE_OPEN_READWRITE, NULL);
     if (rc != SQLITE_OK) {
@@ -20,12 +20,19 @@ int main() {
     }
     sqlite3_stmt *pStmt;
     // jake predmety studenti studoval v roce 2010
+    /*
+    * EXPLAIN QUERY:
+    QUERY PLAN
+    |--SCAN TABLE studuje AS ste
+    |--SEARCH TABLE student AS stt USING INDEX sqlite_autoindex_student_1 (sID=?)
+    `--SEARCH TABLE predmet AS prt USING INDEX sqlite_autoindex_predmet_1 (pID=?)
+    */
     rc = sqlite3_prepare_v2(db,
-                            "Select stt.jmeno, prt.jmeno, ste.rok, ste.body\n"
-                            "FROM student stt\n"
-                            "JOIN studuje ste on stt.sID = ste.sID\n"
-                            "JOIN predmet prt on ste.pID = prt.pID\n"
-                            "where ste.rok = 2010\n",
+                            "EXPLAIN QUERY PLAN Select stt.jmeno, prt.jmeno, ste.rok, ste.body "
+                            "FROM student stt "
+                            "JOIN studuje ste on stt.sID = ste.sID "
+                            "JOIN predmet prt on ste.pID = prt.pID "
+                            "where ste.rok = 2010",
                             -1, &pStmt, NULL);
     if (rc != SQLITE_OK) {
         fprintf(stderr, "sqlite3_prepare_v2() returns %d\n", rc);
@@ -34,13 +41,10 @@ int main() {
 
     printf("\nresult:\n");
     while (SQLITE_ROW == sqlite3_step(pStmt)) {
-        printf("%s | %s | %i | %i \n", sqlite3_column_text(pStmt, 0), sqlite3_column_text(pStmt, 1),
-               sqlite3_column_int(pStmt, 2),
-               sqlite3_column_int(pStmt, 3));
+        printf("%s \n", sqlite3_column_text(pStmt, 3));
     }
-
     sqlite3_finalize(pStmt);
     sqlite3_close(db);
-    printf("\nclose connection");
+    printf("\nclose connection\n");
     return 0;
 }
